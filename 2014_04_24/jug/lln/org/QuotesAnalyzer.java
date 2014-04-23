e70b3322a100c3b6a863cae7c4ab857ef79a1c7a
TRITSCHLER <tritschler@users.noreply.github.com>
2014-04-23 17:36:17 +0200

    Create QuotesAnalyzer.java

---
+++ 2014_04_24/jug/lln/org/QuotesAnalyzer.java
@@ -0,0 +1,28 @@
+package com.tritschler.jcep;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+
+public class QuotesAnalyzer {
+
+
+	public static void main(String[] args) {		
+		if (args.length < 1) {
+			System.out.println("usage: ");
+			System.exit(-1);
+		}
+
+		String[] symbols = args[0].split(",");
+		for (String s : symbols) {		
+			try {
+				History h = History.fromFile(s, args[0] + File.separator + s + ".txt");
+				h.print();
+
+			} catch (NumberFormatException | IOException | ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+		}
+	}
+}
